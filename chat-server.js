f836a42084e4c1d1023255977b8d043635ec6eb2
baldpixels <baldpixels@gmail.com>
2018-03-28 22:00:35 -0500

    socket.username

--- chat-server.js
+++ chat-server.js
@@ -21,22 +21,32 @@ server.listen(3456);
 
 // Do the Socket.IO magic:
 var io = socketio.listen(server);
+
 io.sockets.on("connection", function(socket) {
 	// This callback runs when a new Socket.IO connection is established.
-  console.log("a user connected!");
+  console.log("a new user connected!");
   active_users++;
-  io.sockets.emit("message_to_client", {message: "there are " + active_users + " users currently in the chat room."}) // broadcast the message to other users
+  io.sockets.emit("message_to_client", {
+		username: socket.username,
+		message: "a new user just connected!"
+	}); // broadcast the message to other users
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     active_users--;
     console.log('a user disconnected...');
-    io.sockets.emit("message_to_client", {message: "there are " + active_users + " users currently in the chat room."}) // broadcast the message to other users
+    io.sockets.emit("message_to_client", {
+			username: socket.username,
+			message: "a user disconnected..."
+		}); // broadcast the message to other users
   });
 
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
 
-		console.log("message: "+data["message"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client", {message:data["message"]}) // broadcast the message to other users
+		console.log("message: " + data["message"]); // log it to the Node.JS output
+		io.sockets.emit("message_to_client", {
+			username: socket.username,
+			message: data["message"]
+		}); // broadcast the message to other users
 	});
 });
